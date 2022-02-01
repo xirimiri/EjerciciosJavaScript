5b5561456f9d79048c744dab5cdaf2152fd5de0d
xirimiri <macayaj78@gmail.com>
2022-02-01 10:42:39 -0600

    Do Ejercicio 7: Pares o impares

---
+++ Opcionales/Ejercicio-7/ejercicio7.js
@@ -0,0 +1,8 @@
+function checkNumber(numero) {
+    let resultado = numero % 2;
+    if (resultado == 0) {
+        console.log("El número es par");
+    } else {
+        console.log("El número es impar");
+    }
+};
